8e363499f99d3fb90afcad7a8106a1ddf68b881f
hongshengjie <hongshengjie@live.com>
2023-12-19 22:20:54 +0800

    add alacritty tmux nvim config files

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,49 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util" 
+lspconfig.gopls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go","gomod","gowork","gotmpl"},
+  root_dir = util.root_pattern("go.work","go.mod",".git"),
+})
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+})
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    preferences = {
+      disableSuggestions = true,
+    }
+  },
+  commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports",
+    }
+  }
+}
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,48 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts ={
+      ensure_installed ={
+        "gopls",
+        "rust-analyzer",
+        "typescript-language-server",
+        "eslint-lsp",
+        "prettier",
+      },
+    },
+  },
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.formatter"
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end
+  },
+   {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+
+}
+return plugins
