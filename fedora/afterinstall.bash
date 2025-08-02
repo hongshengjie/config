6312f8bddd2493b5cb2474426204b7b84148c240
hongshengjie <hongshengjie@users.noreply.github.com>
2025-08-02 13:32:30 +0800

    Update afterinstall.bash

--- fedora/afterinstall.bash
+++ fedora/afterinstall.bash
@@ -2,3 +2,14 @@ export LANG=en_US
 xdg-user-dirs-gtk-update
 export LANG=zh_CN.UTF-8
 sudo dnf update
+sudo dnf install alacritty tmux fish neovim
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+
+git clone git@github.com:hongshengjie/config.git
+
+
+
+wget https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.3.2/Clash.Verge-2.3.2-1.x86_64.rpm
